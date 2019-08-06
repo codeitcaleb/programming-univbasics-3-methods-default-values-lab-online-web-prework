6aaaa20f3faaba5f55730d27f0408cd7fa143c85
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-06 21:07:36 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,7 +5,7 @@
 # end  
 
 
-def meal_choice(protein = "meat" veg1, veg2)
+def meal_choice(protein = "meat", veg1, veg2)
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end   
